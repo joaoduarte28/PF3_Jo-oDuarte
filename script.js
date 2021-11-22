cabe3bee5a62c919265ee1e865bc3daae0ccbd2b
joaoduarte28 <78557076+joaoduarte28@users.noreply.github.com>
2021-11-22 20:50:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const calcular = document.getElementById('calcular');
+
+
+function poupanca () {
+    const valoraplicado = document.getElementById('valoraplicado').value;
+    const juro = document.getElementById('juros').value;
+    const parcelas = document.getElementById('parcelas').value;
+    const resultado = document.getElementById('resultado');
+
+    if {
+        valoraplicado = parcelas( 1 + juro) - 1 / juro
+    }
+
+    
+}
+calcular.addEventListener('click', poupanca);
